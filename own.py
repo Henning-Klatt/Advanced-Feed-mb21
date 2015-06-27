525a1335ac270260d6839ddcff4ee70b5c394ab8
Henning Klatt <henning.klatt@web.de>
2015-06-27 11:43:39 +0200

    Create own.py
    Die Header File

---
+++ own.py
@@ -0,0 +1,24 @@
+#Import
+#---------------------------------------------------------------
+import os
+import pickle
+
+
+#Klasse für Feed
+#---------------------------------------------------------------
+class alg:
+    def test():
+        print("YES")
+    
+#Klasse für Errors
+#---------------------------------------------------------------
+class error:
+    def FileDataError():
+        print("File Data Error")
+
+#Klasse für Profile
+#---------------------------------------------------------------
+class pfofile:
+    #Erstellt neues Profil in eine .txt-Datei
+    def create(self, name, feedset, hobbyset):
+        pass
